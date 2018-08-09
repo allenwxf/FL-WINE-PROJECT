9a4292b6160c3d4402d94e47125564510c969503
allenwxf <allen_wangxufeng@163.com>
2018-08-09 20:58:33 +0800

    tmp

--- PricePrediction.py
+++ PricePrediction.py
@@ -1,40 +1,64 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import itertools
 import os
+import math
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 
 from sklearn.preprocessing import LabelEncoder
-
 from tensorflow import keras
 layers = keras.layers
 
 print("You have TensorFlow version", tf.__version__)
 
 data = pd.read_csv("dataset/wine-review/winemag-data-130k-v2.csv")
-data = data[:10000]
-train_size = int(len(data) * 0.8)
+data = data.sample(frac=1)
+print(data.head())
+print(data.shape)
+
+data = data[pd.notnull(data['country'])]
+data = data[pd.notnull(data['price'])]
+data = data.drop(data.columns[0], axis=1)
+
+variety_threshold = 500
+value_counts = data['variety'].value_counts()
+to_remove = value_counts[value_counts <= variety_threshold].index
+data.replace(to_remove, np.nan, inplace=True)
+data = data[pd.notnull(data['variety'])]
+
+# data = data[:60000]
+
+train_size = int(len(data) * .8)
+print("Train size: %d" % train_size)
+print("Test size: %d" % (len(data) - train_size))
 
 # Train features
 description_train = data['description'][:train_size]
 variety_train = data['variety'][:train_size]
+
 # Train labels
 labels_train = data['price'][:train_size]
 
 # Test features
 description_test = data['description'][train_size:]
-description_test.index = range(len(description_test))
+# description_test.index = range(len(description_test))
 variety_test = data['variety'][train_size:]
-variety_test.index = range(len(variety_test))
+# variety_test.index = range(len(variety_test))
+
 # Test labels
 labels_test = data['price'][train_size:]
-labels_test.index = range(len(labels_test))
+# labels_test.index = range(len(labels_test))
 
 
 ################
 ## Wide Model ##
 ################
 # 1. 葡萄酒描述
-vocab_size = 20000          # 词袋数量
+vocab_size = 12000          # 词袋数量
 tokenize = keras.preprocessing.text.Tokenizer(num_words=vocab_size, char_level=False)
 tokenize.fit_on_texts(description_train)
 
@@ -46,8 +70,6 @@ description_bow_test = tokenize.texts_to_matrix(description_test)
 encoder = LabelEncoder()
 encoder.fit(variety_train)
 variety_train = encoder.transform(variety_train)
-encoder = LabelEncoder()
-encoder.fit(variety_test)
 variety_test = encoder.transform(variety_test)
 num_classes = np.max(variety_train) + 1
 # Convert labels to one hot
@@ -61,9 +83,15 @@ merged_layer = layers.concatenate([bow_inputs, variety_inputs])
 merged_layer = layers.Dense(256, activation='relu')(merged_layer)
 predictions = layers.Dense(1)(merged_layer)
 wide_model = keras.Model(inputs=[bow_inputs, variety_inputs], outputs=predictions)
+print(wide_model.summary())
 
 wide_model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
+# wide_model.fit([description_bow_train, variety_train], labels_train, epochs=100, batch_size=128)
+# wide_model.evaluate([description_bow_test, variety_test], labels_test, batch_size=128)
+# predictions = wide_model.predict([description_bow_test, variety_test])
+# print(predictions)
+# exit(0)
 
 ################
 ## Deep Model ##
@@ -72,15 +100,17 @@ train_embed = tokenize.texts_to_sequences(description_train)
 test_embed = tokenize.texts_to_sequences(description_test)
 
 max_seq_length = 170
-train_embed = keras.preprocessing.sequence.pad_sequences(train_embed, maxlen=max_seq_length)
-test_embed = keras.preprocessing.sequence.pad_sequences(test_embed, maxlen=max_seq_length)
+train_embed = keras.preprocessing.sequence.pad_sequences(train_embed, maxlen=max_seq_length, padding="post")
+test_embed = keras.preprocessing.sequence.pad_sequences(test_embed, maxlen=max_seq_length, padding="post")
 
 deep_inputs = layers.Input(shape=(max_seq_length,))
 embedding = layers.Embedding(vocab_size, 8, input_length=max_seq_length)(deep_inputs)
 embedding = layers.Flatten()(embedding)
-
-embed_out = layers.Dense(1, activation='linear')(embedding)
+# embed_out = layers.Dense(1, activation='linear')(embedding)
+embed_out = layers.Dense(1)(embedding)
 deep_model = keras.Model(inputs=deep_inputs, outputs=embed_out)
+print(deep_model.summary())
+
 deep_model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
 
@@ -90,6 +120,8 @@ deep_model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 merged_out = layers.concatenate([wide_model.output, deep_model.output])
 merged_out = layers.Dense(1)(merged_out)
 combined_model = keras.Model(wide_model.input + [deep_model.input], merged_out)
+print(combined_model.summary())
+
 combined_model.compile(loss='mse',optimizer='adam', metrics=['accuracy'])
 
 # Training
@@ -105,8 +137,11 @@ combined_model.evaluate([description_bow_test, variety_test] + [test_embed], lab
 predictions = combined_model.predict([description_bow_test, variety_test] + [test_embed])
 print(predictions)
 
+num_predictions = 40
+diff = 0
 
-for i in range(15):
+for i in range(num_predictions):
     val = predictions[i]
-    print(description_test[i])
-    print(val, 'Actual: ', labels_test.iloc[i], '\n')
+    print(description_test.iloc[i])
+    print('Predicted: ', val[0], 'Actual: ', labels_test.iloc[i], '\n')
+    diff += abs(val[0] - labels_test.iloc[i])
