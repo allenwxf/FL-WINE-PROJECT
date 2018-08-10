ad970ae2e6693baead82ce2e659d03ab03c83880
allenwxf <allen_wangxufeng@163.com>
2018-08-10 19:35:50 +0800

    tmp

--- VarietyPrediction.py
+++ VarietyPrediction.py
@@ -30,7 +30,7 @@ to_remove = value_counts[value_counts <= variety_threshold].index
 data.replace(to_remove, np.nan, inplace=True)
 data = data[pd.notnull(data['variety'])]
 
-# data = data[:60000]
+data = data[:10000]
 
 train_size = int(len(data) * .8)
 print("Train size: %d" % train_size)
@@ -38,17 +38,15 @@ print("Test size: %d" % (len(data) - train_size))
 
 # Train features
 description_train = data['description'][:train_size]
-variety_train = data['variety'][:train_size]
 
 # Train labels
-labels_train = data['price'][:train_size]
+labels_train = data['variety'][:train_size]
 
 # Test features
 description_test = data['description'][train_size:]
-variety_test = data['variety'][train_size:]
 
 # Test labels
-labels_test = data['price'][train_size:]
+labels_test = data['variety'][train_size:]
 
 
 ################
@@ -65,24 +63,32 @@ description_bow_test = tokenize.texts_to_matrix(description_test)
 # 2. 葡萄品种
 # Use sklearn utility to convert label strings to numbered index
 encoder = LabelEncoder()
-encoder.fit(variety_train)
-variety_train = encoder.transform(variety_train)
-variety_test = encoder.transform(variety_test)
-num_classes = np.max(variety_train) + 1
-# Convert labels to one hot
-variety_train = keras.utils.to_categorical(variety_train, num_classes)
-variety_test = keras.utils.to_categorical(variety_test, num_classes)
+encoder.fit(labels_train)
+labels_train = encoder.transform(labels_train)
+labels_test = encoder.transform(labels_test)
+num_classes = np.max(labels_train) + 1
+
+# # Convert labels to one hot
+# labels_train = keras.utils.to_categorical(labels_train, num_classes)
+# labels_test = keras.utils.to_categorical(labels_test, num_classes)
+# print(labels_train[0:10])
+
 
 # 搭建Wide模型
 bow_inputs = layers.Input(shape=(vocab_size,))
-variety_inputs = layers.Input(shape=(num_classes,))
-merged_layer = layers.concatenate([bow_inputs, variety_inputs])
-merged_layer = layers.Dense(256, activation='relu')(merged_layer)
-predictions = layers.Dense(1)(merged_layer)
-wide_model = keras.Model(inputs=[bow_inputs, variety_inputs], outputs=predictions)
+# variety_inputs = layers.Input(shape=(num_classes,))
+# merged_layer = layers.concatenate([bow_inputs, variety_inputs])
+# merged_layer = layers.Dense(256, activation='relu')(merged_layer)
+# predictions = layers.Dense(1)(merged_layer)
+# wide_model = keras.Model(inputs=[bow_inputs, variety_inputs], outputs=predictions)
+inter_layer = layers.Dense(256, activation='relu')(bow_inputs)
+inter_layer = layers.Dense(num_classes)(inter_layer)
+predictions = layers.Activation('softmax')(inter_layer)
+wide_model = keras.Model(input=[bow_inputs], outputs=predictions)
+
 print(wide_model.summary())
 
-wide_model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+wide_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
 ################
@@ -98,35 +104,39 @@ test_embed = keras.preprocessing.sequence.pad_sequences(test_embed, maxlen=max_s
 deep_inputs = layers.Input(shape=(max_seq_length,))
 embedding = layers.Embedding(vocab_size, 8, input_length=max_seq_length)(deep_inputs)
 embedding = layers.Flatten()(embedding)
-# embed_out = layers.Dense(1, activation='linear')(embedding)
-embed_out = layers.Dense(1)(embedding)
+# embed_out = layers.Dense(1)(embedding)
+# deep_model = keras.Model(inputs=deep_inputs, outputs=embed_out)
+embedding = layers.Dense(num_classes)(embedding)
+embed_out = layers.Activation('softmax')(embedding)
 deep_model = keras.Model(inputs=deep_inputs, outputs=embed_out)
 print(deep_model.summary())
 
-deep_model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+deep_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
 ################
 ##### MERGE ####
 ################
 merged_out = layers.concatenate([wide_model.output, deep_model.output])
-merged_out = layers.Dense(1)(merged_out)
+# merged_out = layers.Dense(1)(merged_out)
+merged_out = layers.Dense(num_classes)(merged_out)
+merged_out = layers.Activation('softmax')(merged_out)
 combined_model = keras.Model(wide_model.input + [deep_model.input], merged_out)
 print(combined_model.summary())
 
-combined_model.compile(loss='mse',optimizer='adam', metrics=['accuracy'])
+combined_model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
 
 # Training
-combined_model.fit([description_bow_train, variety_train] + [train_embed], labels_train, epochs=10, batch_size=128)
+combined_model.fit([description_bow_train] + [train_embed], labels_train, epochs=10, batch_size=128)
 
 # Evaluation
-combined_model.evaluate([description_bow_test, variety_test] + [test_embed], labels_test, batch_size=128)
+combined_model.evaluate([description_bow_test] + [test_embed], labels_test, batch_size=128)
 
 
 
 
 
-predictions = combined_model.predict([description_bow_test, variety_test] + [test_embed])
+predictions = combined_model.predict([description_bow_test] + [test_embed])
 print(predictions)
 
 num_predictions = 40
