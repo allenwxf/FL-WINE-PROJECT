17bd6ff69bbdc5a10dea43531c8732831bc58641
allenwxf <allenwxf@gmail.com>
2018-08-13 08:32:23 +0800

    t

--- VarietyPredictionUwsgi.py
+++ VarietyPredictionUwsgi.py
@@ -10,15 +10,16 @@ varietyPrediction = Flask(__name__)
 base_dir="/Users/wxf/Documents/GitHub/FL-WINE-PROJECT/"
 
 data = pd.read_csv(base_dir + "dataset/wine-review/winemag-data-130k-v2-resampled.csv")
-print(data.head(10))
-description_train = data['description'][:]
+all_description = data['description'][:]
 
 encoder = LabelEncoder()
 encoder.fit(data['variety'][:])
 
 vocab_size = 12000  # 词袋数量
 tokenize = keras.preprocessing.text.Tokenizer(num_words=vocab_size, char_level=False)
-tokenize.fit_on_texts(description_train)
+tokenize.fit_on_texts(all_description)
+
+max_seq_length = 170
 
 if os.path.exists(base_dir + "model/variety_prediction.h5"):
     combined_model = keras.models.load_model(base_dir + "model/variety_prediction.h5")
@@ -29,12 +30,19 @@ else:
 def index():
     # request param
     desc = request.args.get("desc")
-    price = request.args.get("price")
+    price = float(request.args.get("price"))
+
+    desc = pd.Series([desc])
+    print(desc)
+    price = pd.Series([price])
 
     description_bow_test = tokenize.texts_to_matrix(desc)
     test_embed = tokenize.texts_to_sequences(desc)
+    test_embed = keras.preprocessing.sequence.pad_sequences(test_embed, maxlen=max_seq_length, padding="post")
 
+    print(combined_model)
     predictions = combined_model.predict([description_bow_test, price] + [test_embed])
+    print(predictions)
     if predictions.size > 0:
         prediction = predictions[0]
         index = np.argmax(prediction)
