9725fb7b1ecca55d088dfa9dffa9daad276b8079
allenwxf <allenwxf@gmail.com>
2018-08-13 23:18:46 +0800

    t

--- VarietyPredictionUwsgi.py
+++ VarietyPredictionUwsgi.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import keras
 from sklearn.preprocessing import LabelEncoder
+from keras import backend as K
 from flask import Flask, request
 varietyPrediction = Flask(__name__)
 
@@ -23,14 +24,16 @@ max_seq_length = 170
 
 
 
-@varietyPrediction.route('/')
+@varietyPrediction.route('/', methods=['GET', 'POST'])
 def index():
+    variety_predicted = ""
+    # if request.method == "POST":
     # request param
-    desc = request.args.get("desc")
-    price = float(request.args.get("price"))
+    desc = request.values.get("desc", "")
+    price = float(request.values.get("price", "0.0"))
+    print(desc, price)
 
     desc = pd.Series([desc])
-    print(desc)
     price = pd.Series([price])
 
     description_bow_test = tokenize.texts_to_matrix(desc)
@@ -44,11 +47,13 @@ def index():
             prediction = predictions[0]
             index = np.argmax(prediction)
             variety_predicted = encoder.inverse_transform(index)
+            K.clear_session()
             return _ret(data={"variety_predicted": variety_predicted})
     else:
         # os.error("模型文件不存在")
         return _ret("模型文件不存在", -1)
-
+    # else:
+    #     return _ret(data={"variety_predicted": variety_predicted})
 
 
 def _ret(msg="", errcode=0, data={}):
