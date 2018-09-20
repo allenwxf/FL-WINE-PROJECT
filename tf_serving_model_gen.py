dd2e6d31282609b1e62e210cbc0cd2d517dc8044
allenwxf <allenwxf@gmail.com>
2018-09-20 16:55:29 +0800

    tensorflow serving model generate

---
+++ tf_serving_model_gen.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+
+# The export path contains the name and the version of the model
+
+tf.keras.backend.set_learning_phase(0)  # Ignore dropout at inference
+
+model = tf.keras.models.load_model('./model/variety_prediction.h5')
+
+print(model.input)
+print(model.outputs)
+
+
+export_path = './model/VarietyPrediction/1'
+
+# Fetch the Keras session and save the model
+# The signature definition is defined by the input and output tensors
+# And stored with the default serving key
+
+with tf.keras.backend.get_session() as sess:
+    tf.saved_model.simple_save(
+        sess,
+        export_path,
+        inputs={'input_bow': model.input[0], 'input_price': model.input[1], 'input_embed': model.input[2]},
+        outputs={'outputs': model.outputs[0]}
+    )
